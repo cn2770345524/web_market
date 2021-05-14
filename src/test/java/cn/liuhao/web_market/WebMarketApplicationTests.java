19c87ca5ef8362a6c046a3a227400b740ee4bdac
cn2770345524 <2770345524@qq.com>
2021-05-14 19:28:21 +0800

    在master上提交的

---
+++ src/test/java/cn/liuhao/web_market/WebMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liuhao.web_market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
